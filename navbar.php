9b3c2b629e025c7998b4ce4084405164ef661e85
Joseph Powell <43303262+Joe-Powell@users.noreply.github.com>
2020-05-09 09:16:18 -0500

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,13 @@
+<nav>
+<ul>
+    <li><a href='index.php'>Home</a></li>
+    <li><a href='index2.php'>Blogs</a></li>
+    <li><a href='beats.php'>Beats</a></li>
+    <li><a href='#'>Fitness</a></li>
+    <li><a href='#'>Web Development</a></li>
+    <form class="logout" action="includes/logout.inc.php" method="post">
+    <button type="submit" name="logout-submit">Logout</button>
+    </form>
+</ul>
+
+</nav>
